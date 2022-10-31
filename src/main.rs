1bcc84d1487defa4f8fa018467e1ca1fe5d7af57
李祖明 <26295028+solace-lee@users.noreply.github.com>
2022-10-31 22:09:59 +0800

    输出错误信息

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,12 @@ fn main() {
     println!("参数为：{:?}", args);
 
     let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("problem parsing arguments: {}", err);
+        eprintln!("problem parsing arguments: {}", err);
         process::exit(1);
     });
 
     if let Err(e) = run(config) {
-        println!("Application error: {}", e);
+        eprintln!("Application error: {}", e);
         process::exit(2);
     };
 }
