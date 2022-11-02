d312babb6a74c565256b5471335bb98d1ea93e74
李祖明 <26295028+solace-lee@users.noreply.github.com>
2022-11-02 22:10:25 +0800

    指针引用

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,16 @@
+use crate::List::{Cons, Nil};
 use my_cargo::{run, Config};
 use std::{env, process};
+enum List {
+    Cons(i32, Box<List>),
+    Nil,
+}
+
 fn main() {
+    let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
+    println!("www,{:#?}", list);
+    return;
+
     let config = Config::new(env::args()).unwrap_or_else(|err| {
         eprintln!("problem parsing arguments: {}", err);
         process::exit(1);
